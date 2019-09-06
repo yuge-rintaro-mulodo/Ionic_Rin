6880ccd0143aaceb84887fbc4da56885a05ea736
yuge-rintaro-mulodo <yuge.rintaro.mulodo@gmail.com>
2019-09-06 16:53:19 +0900

    Ionic part

---
+++ photo-gallery/src/app/tab2/tab2.page.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { PhotoService} from '../services/photo.service';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  currentImage: any;
+
+  constructor(public photoService: PhotoService) {}
+
+  ng0Init() {
+    this.photoService.loadSaved();
+  }
+
+}
+
